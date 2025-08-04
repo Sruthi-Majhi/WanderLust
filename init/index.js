8004fde2e7c43fe7d86f05ec72f4cfa8efdf636e
Sruthi Majhi <sruthimajhi31@gmail.com>
2025-08-05 01:59:03 +0530

    Add new files

--- init/index.js
+++ init/index.js
@@ -1,13 +1,9 @@
 import './style.css';
-import Map from 'ol/Map.js';
-import OSM from 'ol/source/OSM.js';
-import TileLayer from 'ol/layer/Tile.js';
-import View from 'ol/View.js';
 
 const mongoose=require("mongoose");
 const initData=require("./data.js");
 const listing=require("../models/listing.js");
-let mongoUrl="mongodb://127.0.0.1:27017/wanderlust";
+let mongoUrl=process.env.MONGO_ATLAS;
 async function main()
 {
     await mongoose.connect(mongoUrl);
